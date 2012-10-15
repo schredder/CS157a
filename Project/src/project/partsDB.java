6e5116d0545953cce2eecf5277a1f8d53833932a
Yousef Shanawany <to_yshanawany@hotmail.com>
2012-10-14 21:05:19 -0700

    Method has better return type

--- Project/src/project/partsDB.java
+++ Project/src/project/partsDB.java
@@ -2,6 +2,7 @@ package project;
 
 import java.sql.*;
 import java.util.*;
+
 /**
  * Creates and manages a connection to the parts database
  *
@@ -11,10 +12,10 @@ public class partsDB {
 
     private Connection oracleConn;
     private Statement oracleStmt;
-    ArrayList<HashMap> APLBUK =  new ArrayList<HashMap>();
-    HashMap<String,String> column =  new HashMap<String, String>();
-      
-    public partsDB(String connectString, String user, String password) throws SQLException{
+    ArrayList<HashMap> APLBUK = new ArrayList<HashMap>();
+    HashMap<String, String> column = new HashMap<String, String>();
+
+    public partsDB(String connectString, String user, String password) throws SQLException {
 
         try {
             DriverManager.registerDriver(new oracle.jdbc.driver.OracleDriver());
@@ -33,36 +34,35 @@ public class partsDB {
      * @param whereClause
      * @return An arrayList
      */
-    
-      public ArrayList select(String SQLStatement) {
-      /* TODO: Execute the SQL statement, and return it as an ArrayList of
-       tuples represented as HashMaps with the column as the key*/
-     
-      
-      ResultSet rs; 
-      try { 
-          rs = oracleStmt.executeQuery(SQLStatement); 
-          column.put("model",rs.getString(1));
-          column.put("year",rs.getString(2));
-          column.put("desc",rs.getString(3));
-          column.put("litres",rs.getString(4));
-          column.put("engine",rs.getString(5));
-          column.put("inches",rs.getString(6));
-          column.put("rlink",rs.getString(7));
-          APLBUK.add(column);
-      } 
-      
-      catch(SQLException e) { System.out.println("Unable to execute statement: " +
-      SQLStatement + "\nMessage: "+ e.getMessage()); 
- 
-      
-      return null; 
-      //ArrayList<String>; } ResultSetMetaData rsMetaData = rs.getMetaData(); }
-      }
-      return APLBUK; 
-      }
-      
-      
+    public ArrayList<HashMap> select(String SQLStatement) {
+        /*
+         * TODO: Execute the SQL statement, and return it as an ArrayList of
+         * tuples represented as HashMaps with the column as the key
+         */
+
+
+        ResultSet rs;
+        try {
+            rs = oracleStmt.executeQuery(SQLStatement);
+            column.put("model", rs.getString(1));
+            column.put("year", rs.getString(2));
+            column.put("desc", rs.getString(3));
+            column.put("litres", rs.getString(4));
+            column.put("engine", rs.getString(5));
+            column.put("inches", rs.getString(6));
+            column.put("rlink", rs.getString(7));
+            APLBUK.add(column);
+        } catch (SQLException e) {
+            System.out.println("Unable to execute statement: "
+                    + SQLStatement + "\nMessage: " + e.getMessage());
+
+
+            return null;
+            //ArrayList<String>; } ResultSetMetaData rsMetaData = rs.getMetaData(); }
+        }
+        return APLBUK;
+    }
+
     /**
      * Returns a Statement in case something needs to be done outside the scope
      * of this class.
