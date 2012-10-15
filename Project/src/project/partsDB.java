b712abcdd7e08fb699ac332478001d7e438f6c76
chirag arora <samkarora@gmail.com>
2012-10-14 22:29:50 -0700

    fixed changes that yousef made to PARTSDB.
    Parts DB take parameters of your DATABASE URL, username, password. SO
    just change DBTester according to run on your system.

--- Project/src/project/partsDB.java
+++ Project/src/project/partsDB.java
@@ -2,7 +2,6 @@ package project;
 
 import java.sql.*;
 import java.util.*;
-
 /**
  * Creates and manages a connection to the parts database
  *
@@ -12,12 +11,19 @@ public class partsDB {
 
     private Connection oracleConn;
     private Statement oracleStmt;
-    ArrayList<HashMap> APLBUK = new ArrayList<HashMap>();
-    HashMap<String, String> column = new HashMap<String, String>();
+    ArrayList<HashMap> APLBUK =  new ArrayList<HashMap>();
+    HashMap<String,String> column =  new HashMap<String, String>();
+      
+    public partsDB(String connectString, String user, String password) throws SQLException{
 
-    public partsDB(){
-
-        this.oracleConn = Login.oracleConn;
+        try {
+            DriverManager.registerDriver(new oracle.jdbc.driver.OracleDriver());
+            oracleConn = DriverManager.getConnection(connectString, user, password);
+            oracleStmt = oracleConn.createStatement();
+            System.out.println("Connected to DB!");
+        } catch (SQLException ex) {
+            System.err.println(ex.getMessage());
+        }
     }
 
     /**
@@ -27,35 +33,36 @@ public class partsDB {
      * @param whereClause
      * @return An arrayList
      */
-    public ArrayList<HashMap> select(String SQLStatement) {
-        /*
-         * TODO: Execute the SQL statement, and return it as an ArrayList of
-         * tuples represented as HashMaps with the column as the key
-         */
-
-
-        ResultSet rs;
-        try {
-            rs = oracleStmt.executeQuery(SQLStatement);
-            column.put("model", rs.getString(1));
-            column.put("year", rs.getString(2));
-            column.put("desc", rs.getString(3));
-            column.put("litres", rs.getString(4));
-            column.put("engine", rs.getString(5));
-            column.put("inches", rs.getString(6));
-            column.put("rlink", rs.getString(7));
-            APLBUK.add(column);
-        } catch (SQLException e) {
-            System.out.println("Unable to execute statement: "
-                    + SQLStatement + "\nMessage: " + e.getMessage());
-
-
-            return null;
-            //ArrayList<String>; } ResultSetMetaData rsMetaData = rs.getMetaData(); }
-        }
-        return APLBUK;
-    }
-
+    
+      public ArrayList<HashMap> select(String SQLStatement) {
+      /* TODO: Execute the SQL statement, and return it as an ArrayList of
+       tuples represented as HashMaps with the column as the key*/
+     
+      
+      ResultSet rs; 
+      try { 
+          rs = oracleStmt.executeQuery(SQLStatement); 
+          column.put("model",rs.getString(1));
+          column.put("year",rs.getString(2));
+          column.put("desc",rs.getString(3));
+          column.put("litres",rs.getString(4));
+          column.put("engine",rs.getString(5));
+          column.put("inches",rs.getString(6));
+          column.put("rlink",rs.getString(7));
+          APLBUK.add(column);
+      } 
+      
+      catch(SQLException e) { System.out.println("Unable to execute statement: " +
+      SQLStatement + "\nMessage: "+ e.getMessage()); 
+ 
+      
+      return null; 
+      //ArrayList<String>; } ResultSetMetaData rsMetaData = rs.getMetaData(); }
+      }
+      return APLBUK; 
+      }
+      
+      
     /**
      * Returns a Statement in case something needs to be done outside the scope
      * of this class.
